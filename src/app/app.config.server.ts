583107670aed811932a0c2aa1db92c76a98c40c1
sh4iya <shariyahabiba125@gmail.com>
2026-02-02 12:21:18 +0530

    Register interceptor in server config

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,18 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { authInterceptor } from './app/interceptors/auth-interceptor';
+
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(withRoutes(serverRoutes))
+    provideServerRendering(withRoutes(serverRoutes)),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
 
--- src/app/app/interceptors/auth-interceptor.ts
+++ src/app/app/interceptors/auth-interceptor.ts
@@ -2,8 +2,6 @@ import { isPlatformBrowser } from '@angular/common';
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject, PLATFORM_ID } from '@angular/core';
 
-
-
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const platformId = inject(PLATFORM_ID);
 

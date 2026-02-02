c462dde48886f4b506b8136246d95f829219d63c
sh4iya <shariyahabiba125@gmail.com>
2026-02-02 12:00:41 +0530

    Fix interceptor path

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptor } from './app/interceptors/auth-interceptor';
 
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()   
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
 
